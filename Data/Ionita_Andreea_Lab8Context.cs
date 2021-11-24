3eee6ef806ce5f3bab6a436be4ac5bca35817b70
Andreea Ionita <ionitaandreea2608@gmail.com>
2021-11-24 19:23:54 +0200

    Add project files.

---
+++ Data/Ionita_Andreea_Lab8Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ionita_Andreea_Lab8.Models;
+
+namespace Ionita_Andreea_Lab8.Data
+{
+    public class Ionita_Andreea_Lab8Context : DbContext
+    {
+        public Ionita_Andreea_Lab8Context (DbContextOptions<Ionita_Andreea_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ionita_Andreea_Lab8.Models.Book> Book { get; set; }
+    }
+}
